c71cbec3ff34c85801e4c3e5f59f0df242771dc1
Kumar Saurav <mailkr.srv@gmail.com>
2020-06-11 20:15:58 +0530

    Added attention based LSTM

---
+++ lstm_att/att_lstm.py
@@ -0,0 +1,219 @@
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import numpy as np
+import os
+import io
+import time
+
+from .att_module import Encoder, BahdanauAttention, Decoder
+
+def preprocess_sentence(w):
+    w = w.strip()
+    w = re.sub(r'[" "]+', " ", w)
+
+    # replacing everything with space except (ऀ-ॏ, ०-९ ".", "-")
+    w = re.sub(r'[^\u0900-\u094f\u0966-\u096f?,.-]+', "", w)
+
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start>' + w + '<end>'
+    return w
+
+# 1. Remove the accents
+# 2. Clean the sentences
+# 3. Return word pairs in the format: [Word, Lemma]
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+    word_pairs = [[preprocess_sentence(w) for w in l.split()]  
+        for l in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
+def tokenize(lang):
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+        filters='', lower=False)
+    lang_tokenizer.fit_on_texts(lang)
+
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+        padding='post')
+
+    return tensor, lang_tokenizer
+
+def load_dataset(path, num_examples=None):
+    # creating cleaned input, output pairs
+    targ_word, inp_word = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+num_examples = 10000
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(
+    path_to_file, num_examples)
+
+# Calculate max_length of the target tensors
+max_length_targ, max_length_inp = target_tensor.shape[1], input_tensor.shape[1]
+
+# Creating training and validation sets using an 80-20 split
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = \
+    train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# Show length
+print(len(input_tensor_train), len(target_tensor_train), 
+    len(input_tensor_val), len(target_tensor_val))
+
+def convert(lang, tensor):
+    for t in tensor:
+        if t!=0:
+            print ("%d ----> %s" % (t, lang.index_word[t]))
+
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 64
+steps_per_epoch = len(input_tensor_train)//BATCH_SIZE
+embedding_dim = 256
+units = 1024
+vocab_inp_size = len(inp_lang.word_index)+1
+vocab_tar_size = len(targ_lang.word_index)+1
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+
+# sample input
+sample_hidden = encoder.initialize_hidden_state()
+sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+print ('Encoder output shape: (batch size, sequence length, units) {}'.format(sample_output.shape))
+print ('Encoder Hidden state shape: (batch size, units) {}'.format(sample_hidden.shape))
+
+decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE)
+
+sample_decoder_output, _, _ = decoder(tf.random.uniform((BATCH_SIZE, 1)),
+                                      sample_hidden, sample_output)
+
+print ('Decoder output shape: (batch_size, vocab size) {}'.format(sample_decoder_output.shape))
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+@tf.function
+def train_step(inp, targ, enc_hidden):
+    loss = 0
+
+    with tf.GradientTape() as tape:
+        enc_output, enc_hidden = encoder(inp, enc_hidden)
+
+        dec_hidden = enc_hidden
+
+        dec_input = tf.expand_dims([targ_lang.word_index['<start>']] * BATCH_SIZE, 1)
+
+        # Teacher forcing - feeding the target as the next input
+        for t in range(1, targ.shape[1]):
+            # passing enc_output to the decoder
+            predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
+
+            loss += loss_function(targ[:, t], predictions)
+
+            # using teacher forcing
+            dec_input = tf.expand_dims(targ[:, t], 1)
+
+    batch_loss = (loss / int(targ.shape[1]))
+
+    variables = encoder.trainable_variables + decoder.trainable_variables
+
+    gradients = tape.gradient(loss, variables)
+
+    optimizer.apply_gradients(zip(gradients, variables))
+
+    return batch_loss
+
+EPOCHS = 10
+
+for epoch in range(EPOCHS):
+    start = time.time()
+
+    enc_hidden = encoder.initialize_hidden_state()
+    total_loss = 0
+
+    for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+        batch_loss = train_step(inp, targ, enc_hidden)
+        total_loss += batch_loss
+
+        if batch % 100 == 0:
+            print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                   batch,
+                                                   batch_loss.numpy()))
+    # saving (checkpoint) the model every 2 epochs
+    if (epoch + 1) % 2 == 0:
+    checkpoint.save(file_prefix = checkpoint_prefix)
+
+    print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                      total_loss / steps_per_epoch))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+def evaluate(sentence):
+    attention_plot = np.zeros((max_length_targ, max_length_inp))
+
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [inp_lang.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs],
+                                                         maxlen=max_length_inp,
+                                                         padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+
+    result = ''
+
+    hidden = [tf.zeros((1, units))]
+    enc_out, enc_hidden = encoder(inputs, hidden)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']], 0)
+
+    for t in range(max_length_targ):
+        predictions, dec_hidden, attention_weights = decoder(dec_input,
+                                                             dec_hidden,
+                                                             enc_out)
+
+        # storing the attention weights to plot later on
+        attention_weights = tf.reshape(attention_weights, (-1, ))
+        attention_plot[t] = attention_weights.numpy()
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+
+        result += targ_lang.index_word[predicted_id] + ' '
+
+        if targ_lang.index_word[predicted_id] == '<end>':
+            return result, sentence, attention_plot
+
+        # the predicted ID is fed back into the model
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence, attention_plot
+
+def translate(sentence):
+    result, sentence, attention_plot = evaluate(sentence)
+
+    print('Input: %s' % (sentence))
+    print('Predicted translation: {}'.format(result))
+
+    attention_plot = attention_plot[:len(result.split(' ')), :len(sentence.split(' '))]
+    plot_attention(attention_plot, sentence.split(' '), result.split(' '))
